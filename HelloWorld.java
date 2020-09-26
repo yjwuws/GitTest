48b8fc02cad9dd8ff3fc8908c690ba7fb06c7709
yjwuws <60698341+yjwuws@users.noreply.github.com>
2020-09-26 15:41:34 +0800

    Update HelloWorld.java
    add phone

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	String empno;
 	String name;
+	int phone;
     public static void main(String[] args) {
         System.out.println("Hello World.44444444444444444444444444444444!");
 		System.out.println("17");
