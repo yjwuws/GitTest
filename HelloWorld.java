c42eff36c73b76279450997c0efe89b663e895a1
yjwuws <yjwuws@gmail.com>
2020-09-26 14:17:16 +0800

    add hsin

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World.44444444444444444444444444444444!");
 		System.out.println("17");
 		System.out.println("jxn");
+		System.out.println("hsin");
   }
 }
