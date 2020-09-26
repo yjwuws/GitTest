de7fe8d45e3b61a40d5dff85fe31e7d179960e03
yjwuws <yjwuws@gmail.com>
2020-09-26 14:44:16 +0800

    cat edit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("hsin");
 		System.out.println("hhhhhhhhhhhhhhh");
 		System.out.println("dog");
-		System.out.println("master add");
+		System.out.println("cat add"); //cat is enough
   }
   }
 }
